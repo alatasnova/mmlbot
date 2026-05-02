1a404d0c5db14e844cb103223bd5254488bb43de
alta_s <alexei_zvonarev@list.ru>
2026-05-02 11:52:56 +0700

    это так называемый мяу

---
+++ src/main/java/org/am/mmlbot/Mmlbot.java
@@ -0,0 +1,10 @@
+package org.am.mmlbot;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mmlbot implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/am/mmlbot/client/MmlbotClient.java
@@ -0,0 +1,11 @@
+package org.am.mmlbot.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MmlbotClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
